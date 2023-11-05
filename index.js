b627891d64e9b23b05bf1577844f150c990c7b41
Arttu Laitinen <124583634+Arttu73@users.noreply.github.com>
2023-11-05 15:56:41 +0000

    Update index.js

--- index.js
+++ index.js
@@ -76,7 +76,7 @@ async function fetchBreedText(breed) {
     container.appendChild(wikiItem);
   };
 
-  const dogBreeds = ["husky", "beagle", "poodle", "bulldog", "corgi"];
+  const dogBreeds = ["husky", "beagle", "poodle", "bulldog", "corgi", "dingo"];
 
   // Generate five wiki items
   for (const breed of dogBreeds) {
